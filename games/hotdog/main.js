f42f66fc43481d87f5a47500596736c320539497
wuxx1045 <wuxx1045@umn.edu>
2017-02-24 12:46:44 -0600

    basic snake implemented. a little glitchy

--- games/hotdog/main.js
+++ games/hotdog/main.js
@@ -8,8 +8,8 @@ function init() {
     }
 
     ctx.globalCompositeOperation = 'destination-over';
-    mouseX = window.innerWidth/2;
-    mouseY = window.innerHeight/2;
+    mouseX = window.innerWidth / 2;
+    mouseY = window.innerHeight / 2;
 
     document.addEventListener("mousemove", function(event) {
         mouseX = event.clientX;
@@ -17,25 +17,36 @@ function init() {
 
     });
 
-    head_width = 581;
-    head_height = 930;
-    segment_width = 701;
-    segment_height = 501;
-    head =  new Segment('img/head.png', null, window.innerWidth /2, window.innerHeight/2, head_width,head_height);
-    segment = new Segment('img/middle.png', head, window.innerWidth / 2 - head_width, window.innerHeight/2, segment_width, segment_height);
+    head_width = 161;
+    head_height = 258;
+    segment_width = 131;
+    segment_height = 131;
+    end_width = 77;
+    end_height = 132;
 
+    segment_list = [];
 
+    head = new Segment('img/head.png', null, window.innerWidth / 2, window.innerHeight / 2, head_width, head_height, 3);
+    segment = new Segment('img/middle.png', head, head.leadx, head.leady, segment_width, segment_height, 3);
+    segment_list.push(segment);
+    tail = new Segment('img/end.png', segment, segment.leadx, segment.leady, end_width, end_height, 3);
     window.requestAnimationFrame(draw);
+
+
 }
 
 
 function draw() {
-    ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-    head.update(mouseX,mouseY);
+    head.update(mouseX, mouseY);
     head.draw(ctx);
-    // segment.update(0,0);
-    // segment.draw(ctx);
+    for(i=0;i<segment_list.length;i++){
+      segment_list[i].update();
+      segment_list[i].draw(ctx);
+    }
+    tail.update();
+    tail.draw(ctx);
 
     window.requestAnimationFrame(draw);
 }
--- games/hotdog/segment.js
+++ games/hotdog/segment.js
@@ -1,20 +1,40 @@
-function Segment(src, prev, x, y, w, h) {
+function Segment(src, prev, x, y, w, h, speed) {
     this.img = new Image();
     this.img.src = src;
     this.prev = prev;
     this.x = x;
     this.y = y;
-    if (w < h)
-        ratio = 128 / w;
-    else
-        ratio = 128 / h;
-    this.width = w * ratio;
-    this.height = h * ratio;
-    this.magnitude = 3;
+    // if (w < h)
+    //     ratio = 128 / w;
+    // else
+    //     ratio = 128 / h;
+    this.width = w;
+    this.height = h;
+    this.magnitude = speed;
+    this.rotation = 0;
+    this.leadx = this.x + 50;
+    this.leady = this.y;
+    this.leadlength = 50;
+    this.error = 30;
 }
 Segment.prototype.draw = function(ctx) {
-    ctx.drawImage(this.img, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
 
+    ctx.save();
+    ctx.translate(this.x,this.y);
+    ctx.rotate(this.rotation);
+    ctx.drawImage(this.img, - this.width / 2,- this.height / 2, this.width, this.height);
+    ctx.restore();
+
+    px = this.leadlength * Math.cos(this.rotation);
+    py = this.leadlength * Math.sin(this.rotation);
+
+    this.leadx = this.x + px;
+    this.leady = this.y + py;
+
+    ctx.beginPath();
+    ctx.moveTo(this.x,this.y);
+    ctx.lineTo(this.leadx,this.leady);
+    ctx.stroke();
 }
 //update x y towards the previous segments location
 Segment.prototype.update = function(x, y) {
@@ -22,25 +42,27 @@ Segment.prototype.update = function(x, y) {
         targX = x;
         targY = y;
     } else {
-        targX = this.prev.x;
-        targY = this.prev.y;
+        targX = this.prev.leadx;
+        targY = this.prev.leady;
     }
 
-    diffx = Math.abs(this.x - targX);
-    diffy = Math.abs(this.y - targY);
+    var diffx = this.x - targX;
+    var diffy = this.y - targY;
 
-    angle = Math.atan2(diffy, diffx);
-    console.log(angle);
+    var rotationRadian = Math.atan2(diffy, diffx);
+    rotationRadian += rotationRadian < 0 ? 2*Math.PI : 0;
+    var rotationDegree = rotationRadian*(180/Math.PI);
+  //  rotationDegree += rotationDegree < 0 ? 360 : 0;
+    // console.log("radian: "+rotationRadian);
+    // console.log("angle: "+rotationDegree);
 
-    incx = this.magnitude * Math.cos(angle);
-    incy = this.magnitude * Math.sin(angle);
+    this.rotation = rotationRadian;
 
+    var incx = this.magnitude * Math.cos(rotationRadian);
+    var incy = this.magnitude * Math.sin(rotationRadian);
 
-    incx = targX < this.x ? -incx : incx;
-    incy = targY < this.y ? -incy : incy;
-
-    incx = diffx < 5 ? 0 : incx;
-    incy = diffy < 5 ? 0 : incy;
+    incx = Math.abs(diffx) < this.error ? 0 : incx;
+    incy = Math.abs(diffy) < this.error ? 0 : incy;
 
     // if (this.x - this.width / 2 - incx < 0 && targX <= this.x || this.x + this.width / 2 + incx > window.innerWidth && targX >= window.innerWidth - 5) {
     //     incx = 0;
@@ -48,6 +70,6 @@ Segment.prototype.update = function(x, y) {
     // if (this.y - this.height / 2 - incy < 0 && targY <= this.y || this.y + this.height / 2 + incy > window.innerHeight && targY >= window.innerHeight - 5) {
     //     incy = 0;
     // }
-    this.x += incx;
-    this.y += incy;
+    this.x -= incx;
+    this.y -= incy;
 }
