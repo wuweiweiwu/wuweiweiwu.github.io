ee2c0310899be428a710d1def7bc694e650c71cc
hungweiwu <wuxx1045@umn.edu>
2017-08-28 15:17:36 -0700

    Formatting and updating bio

--- background.js
+++ background.js
@@ -1,57 +1,57 @@
 function Background(img, width, height, per_row, per_column) {
-    this.img = img;
-    this.width = width;
-    this.height = height;
-    this.per_row = per_row;
-    this.per_column = per_column;
-    this.room_width = this.width / this.per_row;
-    this.room_height = this.height / this.per_column;
-    //start at center
-    this.current_room = 4;
-    this.current_room_x = this.room_width;
-    this.current_room_y = this.room_height;
-    this.target_room_x = this.current_room_x;
-    this.target_room_y = this.current_room_y;
+  this.img = img;
+  this.width = width;
+  this.height = height;
+  this.per_row = per_row;
+  this.per_column = per_column;
+  this.room_width = this.width / this.per_row;
+  this.room_height = this.height / this.per_column;
+  //start at center
+  this.current_room = 4;
+  this.current_room_x = this.room_width;
+  this.current_room_y = this.room_height;
+  this.target_room_x = this.current_room_x;
+  this.target_room_y = this.current_room_y;
 }
 Background.prototype.toRoom = function(targetRoom, x, y, w, h) {
-    if (targetRoom < 0 || targetRoom >= 9)
-        return false;
-
-    var targetRow = Math.floor(targetRoom / this.per_row);
-    var targetColumn = Math.floor(targetRoom % this.per_row)
-
-    var currentRow = Math.floor(this.current_room / this.per_row);
-    var currentColumn = Math.floor(this.current_room % this.per_row);
-
-    if ((targetRow == currentRow || targetColumn == currentColumn) && this.withinDoorway(x, y, w, h)) {
-        this.target_room_x = targetColumn * this.room_width;
-        this.target_room_y = targetRow * this.room_height;
-        this.current_room = targetRoom;
-        return true;
-    }
+  if (targetRoom < 0 || targetRoom >= 9)
     return false;
+
+  var targetRow = Math.floor(targetRoom / this.per_row);
+  var targetColumn = Math.floor(targetRoom % this.per_row)
+
+  var currentRow = Math.floor(this.current_room / this.per_row);
+  var currentColumn = Math.floor(this.current_room % this.per_row);
+
+  if ((targetRow == currentRow || targetColumn == currentColumn) && this.withinDoorway(x, y, w, h)) {
+    this.target_room_x = targetColumn * this.room_width;
+    this.target_room_y = targetRow * this.room_height;
+    this.current_room = targetRoom;
+    return true;
+  }
+  return false;
 }
 Background.prototype.withinDoorway = function(person_x, person_y, person_width, person_height) {
-    return ((person_x < window.innerWidth / 2 + person_width / 2 && person_x > window.innerWidth / 2 - person_width / 2) || (person_y < window.innerHeight / 2 + person_width / 2 && person_y > window.innerHeight / 2 - person_width / 2));
+  return ((person_x < window.innerWidth / 2 + person_width / 2 && person_x > window.innerWidth / 2 - person_width / 2) || (person_y < window.innerHeight / 2 + person_width / 2 && person_y > window.innerHeight / 2 - person_width / 2));
 }
 Background.prototype.draw = function(ctx) {
-    ctx.drawImage(this.img, this.current_room_x, this.current_room_y, this.room_width, this.room_height, 0, 0, window.innerWidth, window.innerHeight);
-    var diffx = 40 * (this.current_room_x - this.target_room_x) / (Math.abs(this.current_room_x - this.target_room_x));
-    var diffy = 40 * (this.current_room_y - this.target_room_y) / (Math.abs(this.current_room_y - this.target_room_y));
-    if (Math.abs(this.current_room_x - this.target_room_x) < diffx) {
-        diffx = (this.current_room_x - this.target_room_x) / (Math.abs(this.current_room_x - this.target_room_x));
-    }
-    if (Math.abs(this.current_room_y - this.target_room_y) < diffy) {
-        diffy = (this.current_room_y - this.target_room_y) / (Math.abs(this.current_room_y - this.target_room_y));
-    }
+  ctx.drawImage(this.img, this.current_room_x, this.current_room_y, this.room_width, this.room_height, 0, 0, window.innerWidth, window.innerHeight);
+  var diffx = 40 * (this.current_room_x - this.target_room_x) / (Math.abs(this.current_room_x - this.target_room_x));
+  var diffy = 40 * (this.current_room_y - this.target_room_y) / (Math.abs(this.current_room_y - this.target_room_y));
+  if (Math.abs(this.current_room_x - this.target_room_x) < diffx) {
+    diffx = (this.current_room_x - this.target_room_x) / (Math.abs(this.current_room_x - this.target_room_x));
+  }
+  if (Math.abs(this.current_room_y - this.target_room_y) < diffy) {
+    diffy = (this.current_room_y - this.target_room_y) / (Math.abs(this.current_room_y - this.target_room_y));
+  }
 
-    if (Math.abs(this.current_room_x - this.target_room_x) >= diffx) {
-        this.current_room_x -= diffx;
-        return false;
-    }
-    if (Math.abs(this.current_room_y - this.target_room_y) >= diffy) {
-        this.current_room_y -= diffy;
-        return false;
-    }
-    return true;
+  if (Math.abs(this.current_room_x - this.target_room_x) >= diffx) {
+    this.current_room_x -= diffx;
+    return false;
+  }
+  if (Math.abs(this.current_room_y - this.target_room_y) >= diffy) {
+    this.current_room_y -= diffy;
+    return false;
+  }
+  return true;
 }
--- sprites.js
+++ sprites.js
@@ -1,42 +1,42 @@
 function Sprites(src, width, height, per_row, per_column) {
-    this.img = new Image();
-    this.img.src = src;
-    this.speed = 7;
-    this.counter = 0;
-    this.current = 0;
-    this.per_row = per_row;
-    this.per_column = per_column;
-    this.width = width;
-    this.height = height;
+  this.img = new Image();
+  this.img.src = src;
+  this.speed = 7;
+  this.counter = 0;
+  this.current = 0;
+  this.per_row = per_row;
+  this.per_column = per_column;
+  this.width = width;
+  this.height = height;
 }
 Sprites.prototype.updateWalkLeft = function() {
-    if (this.counter == this.speed - 1) {
-        this.current = (this.current + 1) % per_row;
-        this.current += per_row;
-    }
-    this.counter = (this.counter + 1) % this.speed;
+  if (this.counter == this.speed - 1) {
+    this.current = (this.current + 1) % per_row;
+    this.current += per_row;
+  }
+  this.counter = (this.counter + 1) % this.speed;
 }
 Sprites.prototype.updateWalkRight = function() {
-    if (this.counter == this.speed - 1) {
-        this.current = (this.current + 1) % per_row;
-    }
-    this.counter = (this.counter + 1) % this.speed;
+  if (this.counter == this.speed - 1) {
+    this.current = (this.current + 1) % per_row;
+  }
+  this.counter = (this.counter + 1) % this.speed;
 }
 Sprites.prototype.updateStandLeft = function() {
-    this.current = 13;
-    this.counter = 0;
+  this.current = 13;
+  this.counter = 0;
 }
 Sprites.prototype.updateStandRight = function() {
-    this.current = 12;
-    this.counter = 0;
+  this.current = 12;
+  this.counter = 0;
 }
 Sprites.prototype.draw = function(ctx, x, y) {
-    var row = Math.floor(this.current / this.per_row);
-    var column = Math.floor(this.current % this.per_row);
+  var row = Math.floor(this.current / this.per_row);
+  var column = Math.floor(this.current % this.per_row);
 
-    //x and y are center
-    var frame_width = this.width / this.per_row;
-    var frame_height = this.height / this.per_column;
+  //x and y are center
+  var frame_width = this.width / this.per_row;
+  var frame_height = this.height / this.per_column;
 
-    ctx.drawImage(this.img, frame_width * column, frame_height * row + 3, frame_width, frame_height - 3, x - frame_width / 2, y - frame_height / 2, frame_width, frame_height);
+  ctx.drawImage(this.img, frame_width * column, frame_height * row + 3, frame_width, frame_height - 3, x - frame_width / 2, y - frame_height / 2, frame_width, frame_height);
 }
